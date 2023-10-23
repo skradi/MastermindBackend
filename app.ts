8f030980f9c9e0f87cd4ab48fd4a359bf9e63544
Hubert Leszczynski <hubert.leszczynskiii@gmail.com>
2023-10-23 23:21:47 +0200

    Init Commit backend

---
+++ app.ts
@@ -0,0 +1,20 @@
+import express, {json} from 'express';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
+app.use(json());
+
+
+app.get('/', async (req,res)=>{
+    res.send('hi');
+})
+
+app.listen(3001, '0.0.0.0', ()=>{
+    console.log("listening on port http://localhost:3001");
+});
